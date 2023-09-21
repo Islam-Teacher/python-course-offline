2d1acada1c1fb38306c809a55a68479563b5da7c
Islam <144232473+Islam-Teacher@users.noreply.github.com>
2023-09-21 09:07:09 +0300

    Add files via upload

--- Второй урок/slice.py
+++ Второй урок/slice.py
@@ -2,14 +2,19 @@
 
 name = "Islam Teacher"
 
+a = "AaBbCcDd"
+print(a[::2])
 
+print(name[0:5])
 print(name[0:-1])
 last_name = name[5:]
-funky_name = name[0:8:2]
-#print(funky_name)
+funky_name = name[::2]
+print(funky_name)
 reversed_name = name[::-1]
 print(reversed_name)
 
 website1 = "http://google.com"
+
+#google
 
 
